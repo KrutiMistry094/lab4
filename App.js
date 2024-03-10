de0341c46ab9e944841170cd0327682fdad785e4
KrutiMistry094 <krutirajnikant.mistry@edu.sait.ca>
2024-03-10 15:53:49 -0600

    lab4

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/**
+ * My To Do List App
+ *
+ * @format
+ */
 
-export default function App() {
+import React from "react";
+import { SafeAreaView, StyleSheet } from "react-native";
+import ToDoForm from "./ToDoForm";
+import ToDoList from "./ToDoList";
+import { useState } from "react";
+
+function App() {
+  const [tasks, setTasks] = useState(["Do laundry", "Go to gym", "Walk dog"]);
+  const addTask = (taskTest) => {
+    setTasks([...tasks, taskTest]);
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <ToDoForm addTask={addTask} />
+      <ToDoList tasks={tasks} />
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ ToDoForm.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { StyleSheet, View, TextInput, Button } from "react-native";
+import { useState } from "react";
+export default function ToDoForm({ addTask }) {
+  const [taskText, setTaskText] = useState("");
+
+  return (
+    <View style={styles.form}>
+      <TextInput
+        style={styles.input}
+        placeholder="Add a new task..."
+        onChangeText={(text) => setTaskText(text)}
+        value={taskText}
+      />
+      <Button title="Add" onPress={() => addTask(taskText)} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    marginRight: 10,
+    paddingTop: 14,
+  },
+  form: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginHorizontal: 20,
+    marginTop: 20,
+  },
+});
